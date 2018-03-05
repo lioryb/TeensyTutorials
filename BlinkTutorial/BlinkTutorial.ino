8e3290455f514befee981c38f8fd0594d2156a13
Lior Y. Benjamin <lioryb@gmail.com>
2018-03-05 17:30:21 +0200

    added blinking functions
    moved serial setup to a function with LED indications

--- BlinkTutorial/BlinkTutorial.ino
+++ BlinkTutorial/BlinkTutorial.ino
@@ -16,6 +16,78 @@
 
 int gCounter;                  // global variable for counter
 
+/******************************************************************************/ 
+/*  Function name: blinkOnce                                                  */
+/*  Function description: makes the LED blink once, assuming that the LED pin */
+/*                        is already set to OUTPUT mode                       */
+/*  Input Params: int duration in miliseconds                                 */
+/*  Output Params:                                                            */
+/*  Return value:                                                             */
+/******************************************************************************/
+void blinkOnce(int duration, bool keepOn)
+{
+    digitalWrite(LED, HIGH);   // turn on the LED. HIGH is the voltage level
+    delay(duration);
+    if(!keepOn)
+    {
+       digitalWrite(LED, LOW);   // turn off the LED. LOW is the voltage level
+    }
+}
+
+/******************************************************************************/ 
+/*  Function name: multiBlink                                                 */
+/*  Function description: makes the LED blink multiple times, according the   */
+/*                        given number of times                               */
+/*                        This function assumes the LED pinmode is set to OUT */
+/*  Input Params: int duration in miliseconds for both on and off time        */
+/*                int number of times to blink                                */
+/*  Output Params:                                                            */
+/*  Return value:                                                             */
+/******************************************************************************/
+void multiBlink(int durationOn, int durationOff, int times)
+{
+    int loopCounter;
+    
+    for(loopCounter = 0 ; loopCounter < times ; loopCounter++)
+    {
+        blinkOnce(durationOn, false);
+        delay(durationOff);
+    }
+}
+
+
+/******************************************************************************/
+/*  Function name: startSerial                                                */
+/*  Function description: starts the serial port and waits for it             */
+/*  Input Params: int baudrate                                                */
+/*  Output Params:                                                            */
+/*  Return value: boolean for success                                         */
+/******************************************************************************/
+int startSerial(int buadrate)
+{
+    int serialTimeOut = 0;
+    int returnValue = -1;
+    
+    multiBlink(100, 100, 4);
+    blinkOnce(100, true);
+    Serial.begin(buadrate);
+    while(!Serial && serialTimeOut<50)
+    {
+        // wait
+        delay(100);
+        serialTimeOut++;
+    }
+    
+    if(Serial)
+        returnValue = 0;
+    else
+        multiBlink(200, 200, 2);
+
+        
+    return returnValue;
+    
+}
+
 
 /******************************************************************************/
 /*  Function name: setup                                                      */
@@ -26,8 +98,7 @@ int gCounter;                  // global variable for counter
 void setup()
 {
     pinMode(LED, OUTPUT);   // set LED pin to 
-    Serial.begin(BAUDRATE);
-    gCounter = 0;
+    gCounter = startSerial(BAUDRATE);
 }
 
 /******************************************************************************/
@@ -40,22 +111,25 @@ void loop()
 {
     String locString;
     
-    locString = "Turning LED on for ";
-    locString += ONTIME;
-    locString += "milisec. counter value: ";
-    locString += gCounter;
-    Serial.println(locString);
-    digitalWrite(LED, HIGH);   // turn on the LED. HIGH is the voltage level
-    delay(ONTIME);
+    while(gCounter >= 0 && gCounter < 200)
+    {
+        locString = "Turning LED on for ";
+        locString += ONTIME;
+        locString += "milisec. counter value: ";
+        locString += gCounter;
+        Serial.println(locString);
+        digitalWrite(LED, HIGH);   // turn on the LED. HIGH is the voltage level
+        delay(ONTIME);
+        
+        locString = "Turning LED off for ";
+        locString += OFFTIME;
+        locString += "milisec. counter value is yet: ";
+        locString += gCounter;
+        Serial.println(locString);
+        digitalWrite(LED, LOW);   // turn off the LED. LOW is the voltage level
+        delay(OFFTIME);
     
-    locString = "Turning LED off for ";
-    locString += OFFTIME;
-    locString += "milisec. counter value is yet: ";
-    locString += gCounter;
-    Serial.println(locString);
-    digitalWrite(LED, LOW);   // turn off the LED. LOW is the voltage level
-    delay(OFFTIME);
-    
-    gCounter++;
+        gCounter++;
+    }
 }
 
